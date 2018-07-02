fd88d23b57f24af87ecf97cffc4af7abf0be607d
Luoana Chirita <luoana.livadariu@gmail.com>
2018-07-02 08:18:06 +0000

    deli

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,27 @@
-# Write your code here.
+def take_a_number(katz_deli, name)
+  katz_deli.push(name)
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+end
+
+def line(katz_deli)
+  if katz_deli.length > 0
+    message = "The line is currently:"
+    katz_deli.each_with_index do |name, index|
+      message = message + " #{index + 1}. #{name}"
+    end 
+    puts message
+  else
+    puts "The line is currently empty."
+  end
+end
+
+def now_serving(katz_deli)
+  if katz_deli.length > 0 
+    puts "Currently serving #{katz_deli[0]}."
+    katz_deli.shift
+    katz_deli
+  else
+    puts "There is nobody waiting to be served!"
+  end
+end
+
