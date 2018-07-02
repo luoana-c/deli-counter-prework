4a37b6873aa07b351bdb4a6b74828bf0e1462632
Luoana Chirita <luoana.livadariu@gmail.com>
2018-07-02 08:06:50 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,17 @@
 def take_a_number(katz_deli, name)
   katz_deli.push(name)
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
-    
 end
 
 def line(deli_line)
   
+end
+
+def now_serving(katz_deli)
+  if katz_deli.length > 0 
+    puts "Currently serving #{katz_deli[0]}."
+    katz_deli.
+    
+  end
 end
 
