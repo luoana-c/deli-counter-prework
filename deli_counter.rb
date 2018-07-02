225b3c5f294032a0d736f6d28d0f7b4c553c59b6
Luoana Chirita <luoana.livadariu@gmail.com>
2018-07-02 08:04:33 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,7 @@
-katz_deli = []
-
-def take_a_number(deli_line)
-  
+def take_a_number(katz_deli, name)
+  katz_deli.push(name)
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+    
 end
 
 def line(deli_line)
