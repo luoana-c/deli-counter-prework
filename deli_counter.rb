c69eb8c7016c556be508d50499a141777c6d5c7f
Luoana Chirita <luoana.livadariu@gmail.com>
2018-07-02 08:13:10 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,6 +8,7 @@ def line(katz_deli)
     message = "The line is currently:"
     katz_deli.each_with_index do |name, index|
       message = message + " #{index + 1}. #{name}"
+      message
     end
   else
     puts "The line is currently empty."
