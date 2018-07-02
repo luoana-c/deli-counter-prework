acb72d53f6d650cf609c294f401f0d84b8ce4dd6
Luoana Chirita <luoana.livadariu@gmail.com>
2018-07-02 08:07:43 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -10,8 +10,11 @@ end
 def now_serving(katz_deli)
   if katz_deli.length > 0 
     puts "Currently serving #{katz_deli[0]}."
-    katz_deli.
-    
-  end
+    katz_deli.shift
+    katz_deli
+  
+  else
+    puts "There is nobody waiting to be served!"
+  
 end
 
